c0ee4b1155464d0db483a463fb99525c574c4a96
ericsallinger <eric.sallinger303@gmail.com>
2023-07-16 12:16:18 -0600

    init react

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css"; // Import the CSS you wrote
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to the Home Page</h1>
+      {/* More HTML and components go here */}
+    </div>
+  );
+};
+
+export default Home;
